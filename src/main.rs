0bcd1534c9dbdfd61633848dbca852a25e80d64a
Eduardo León <edo.leon.g@gmail.com>
2020-06-12 22:19:12 -0400

    first commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+#[macro_use]
+extern crate actix_web;
+
+use actix_web::{App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    println!("GET: /");
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/again")]
+async fn again() -> impl Responder {
+    println!("GET: /again");
+    HttpResponse::Ok().body("Hello world again!")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    println!("Starting actix-web server");
+
+    HttpServer::new(|| App::new().service(index).service(again))
+        .bind("0.0.0.0:5000")?
+        .run()
+        .await
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn sample_test() {
+        assert!(true);
+    }
+}
