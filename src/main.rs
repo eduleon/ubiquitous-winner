a263d954cd5641f5f6283c1f4845817244ba63e8
Eduardo León <edo.leon.g@gmail.com>
2020-06-13 00:33:57 -0400

    Add http client to retrieve online indicators

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate actix_web;
+extern crate ureq;
 
 use actix_web::{App, HttpResponse, HttpServer, Responder};
 
@@ -12,14 +13,26 @@ async fn index() -> impl Responder {
 #[get("/again")]
 async fn again() -> impl Responder {
     println!("GET: /again");
-    HttpResponse::Ok().body("Hello world again!")
+    HttpResponse::Ok().body("Hello world again :B!")
+}
+
+#[get("/indecon")]
+async fn indecon() -> impl Responder {
+    let resp = ureq::get("https://www.indecon.online/")
+        .set("X-My-Header", "Secret")
+        .call();
+
+    let json = resp.into_json().unwrap();
+    println!("json: {:?}", json);
+
+    HttpResponse::Ok().json(json)
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    println!("Starting actix-web server");
+    println!("Starting actix-web server (port: 5000)");
 
-    HttpServer::new(|| App::new().service(index).service(again))
+    HttpServer::new(|| App::new().service(index).service(again).service(indecon))
         .bind("0.0.0.0:5000")?
         .run()
         .await
